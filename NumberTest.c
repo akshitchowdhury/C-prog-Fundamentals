2bcb3b9c01d86375535b5154bdb17c3c89f01490
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-24 00:03:09 +0530

    dumpAll

---
+++ NumberTest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// int main() {
+//     int num = 25;
+
+
+//     printf("The number here is %d", num);
+
+    
+//     return 0;
+// } 
+
+int main(){
+
+    char name[] = "Shura";
+
+    printf("The name is %s\n", name);
+
+    return 0;
+}
